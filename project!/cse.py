1361a438288698ab72a950c5e208e1b9a070cb87
Sai Mohan Gajapaka <46029083+saimohan16@users.noreply.github.com>
2023-09-22 17:53:20 -0400

    Add files via upload

---
+++ project!/cse.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep  6 08:44:15 2023
+
+@author: saimo
+"""
+
+import streamlit as st
+import seaborn as sns
+import pandas as pd
+df = pd.read_csv(r"D:\Ph.D\Semester-III\CMSE-830\In-class Assignments\Cancer_data.csv")
+st.write("plots for the cancer dataset")
+X_axis = st.text_input('input for X-axis ')
+Y_axis = st.text_input('input for Y-axis')
+
+#the plot shows the radius mean of the B and M diagnos
+g = sns.catplot(x= X_axis, y= Y_axis, data=df)
+
+st.pyplot(g)

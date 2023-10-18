9b3e374addeb825ac27d4ce50b45234ba0c4ceb1
Sai Mohan Gajapaka <46029083+saimohan16@users.noreply.github.com>
2023-10-17 21:15:33 -0400

    Update app.py

--- Framingham_app/app.py
+++ Framingham_app/app.py
@@ -11,15 +11,46 @@ import altair as alt
 
 import streamlit as st
 
-# Add custom CSS to change background color
 st.markdown(
-    <style>
-    body {
-        background-color: #f0f0f0;  /* Change this color to your desired background color */
-    }
-    </style>
-    unsafe_allow_html=True
+    """
+<style>
+.reportview-container .markdown-text-container {
+    font-family: monospace;
+}
+.sidebar .sidebar-content {
+    background-image: linear-gradient(#2e7bcf,#2e7bcf);
+    color: white;
+}
+.Widget>label {
+    color: white;
+    font-family: monospace;
+}
+[class^="st-b"]  {
+    color: white;
+    font-family: monospace;
+}
+.st-bb {
+    background-color: transparent;
+}
+.st-at {
+    background-color: #0c0080;
+}
+footer {
+    font-family: monospace;
+}
+.reportview-container .main footer, .reportview-container .main footer a {
+    color: #0c0080;
+}
+header .decoration {
+    background-image: none;
+}
+
+</style>
+""",
+    unsafe_allow_html=True,
 )
+
+
 # Load the Framingham dataset (you'll need to replace 'framingham.csv' with the actual dataset file path)
 data = pd.read_csv('Framingham_app/framingham.csv')
 

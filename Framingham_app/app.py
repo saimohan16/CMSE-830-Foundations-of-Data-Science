950be2d114e5979c739fb3edef239bda3a40ee5c
Sai Mohan Gajapaka <46029083+saimohan16@users.noreply.github.com>
2023-12-09 00:20:03 -0500

    Update app.py

--- Framingham_app/app.py
+++ Framingham_app/app.py
@@ -244,8 +244,8 @@ with tab2:
     # st.pyplot()
     # st.header("A dog")
     # st.image("https://static.streamlit.io/examples/dog.jpg", width=200)
-    st.subheader("Conclusion")
-    st.write(" From the above analysis we can clearly see how the data is distributed and how the missing values look after imputation.")
+    #st.subheader("Conclusion")
+    #st.write(" From the above analysis we can clearly see how the data is distributed and how the missing values look after imputation.")
     
 
 with tab3:
